6b951c184bb91cc1a8beb9d06d538662321cbfa9
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2026-06-04 19:23:02 +0800

    启用java25降级支持

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,9 +11,3 @@ minecraft {
         "-XX:+UseCompactObjectHeaders"
     )
 }
-
-dependencies {
-    compileOnly("org.projectlombok:lombok:1.18.32")
-    annotationProcessor("org.projectlombok:lombok:1.18.32")
-    implementation("org.ow2.asm:asm-commons:9.2")
-}
